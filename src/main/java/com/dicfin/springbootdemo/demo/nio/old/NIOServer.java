ac762bdfd7862171bd8c9648c8caaaf6976aa06f
wangjinglei <it_wjl@163.com>
2021-08-24 09:28:30 +0800

    NIO learning

--- src/main/java/com/dicfin/springbootdemo/demo/nio/old/NIOServer.java
+++ src/main/java/com/dicfin/springbootdemo/demo/nio/old/NIOServer.java
@@ -2,9 +2,11 @@ package com.dicfin.springbootdemo.demo.nio.old;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class NIOServer {
@@ -19,10 +21,22 @@ public class NIOServer {
 
         while (true) {
             SocketChannel socketChannel = serverSocketChannel.accept();
-            if (socketChannel != null){
+            if (socketChannel != null) {
                 System.out.println("连接成功");
                 socketChannel.configureBlocking(false);
                 channelList.add(socketChannel);
+            }
+            Iterator<SocketChannel> iterator = channelList.iterator();
+            while (iterator.hasNext()) {
+                SocketChannel channel = iterator.next();
+                ByteBuffer allocate = ByteBuffer.allocate(1024);
+                int read = channel.read(allocate);
+                if (read > 0) {
+                    System.out.println("接受到数据："+new String(allocate.array()));
+                } else if (read == -1){
+                    iterator.remove();
+                    System.out.println("客户端连接断开");
+                }
             }
         }
     }
